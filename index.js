df4d2eeadd3960e644c22ebfa617ab58f2c26e08
Siddhartha <siddpurwar@gmail.com>
2022-03-26 17:28:05 +0530

    Bug in index.js
    Fill Password function not working and
    Password generator function not giving proper Password

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
-function getPassword() {
-    alert("Getting Password");
+let pswd1 = document.getElementById("pswd-1");
+let pswd2 = document.getElementById("pswd-2");
+let pswd3 = document.getElementById("pswd-3");
+let pswd4 = document.getElementById("pswd-4");
+
+
+
+function fillPasswords() {
+    pswd1.innerText = getPassword();
+    pswd2.innerText = getPassword();
+    pswd3.innerText = getPassword();
+    pswd4.innerText = getPassword();
 }
 
+
+
+function getPassword() {
+    let passwordContent = [];
+    let length = getLength();
+    let generator = [getLowerChar(), getUpperChar(), getDigit(), getSpecial()];
+
+    passwordContent.push(String.fromCharCode(generator[0]));
+    passwordContent.push(String.fromCharCode(generator[1]));
+    passwordContent.push(String.fromCharCode(generator[2]));
+    passwordContent.push(String.fromCharCode(generator[3]));
+
+    for (let i = 0; i < length - 4; i++) {
+        let index = Math.floor(Math.random() * 4);
+        passwordContent.push(String.fromCharCode(generator[index]));
+    }
+
+    passwordContent = passwordContent.sort(()=>Math.random()-0.5);
+
+    let password = "";
+    for (let i = 0; i < passwordContent.length; i++) {
+        password += passwordContent[i];
+    }
+
+    return password;
+}
+
+
+
+function getLength() {
+    return Math.floor((Math.random() * 8)) + 8;
+}
+
+function getLowerChar() {
+    return "a".charCodeAt(0) + Math.floor((Math.random() * 26));
+}
+
+function getUpperChar() {
+    return "A".charCodeAt(0) + Math.floor((Math.random() * 26));
+}
+
+function getDigit() {
+    return ("0".charCodeAt(0) + Math.floor(Math.random() * 10));
+}
+
+function getSpecial() {
+    return ("!".charCodeAt(0) + Math.floor(Math.random() * 34));
+}
